2523febc3bcec0fae9be08d105bb5a86fc01ea5e
Xiolin Ramirez <66921726+X-ramirez@users.noreply.github.com>
2021-03-11 21:50:26 -0400

    primer commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HELLO_WORD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
